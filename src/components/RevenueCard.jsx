b2833bc67e34fc79bcea199762d638519ef5e801
SyedGMIbrahim <syedgibrahim@gmail.com>
2025-02-03 22:35:14 +0530

    Modified

--- src/components/RevenueCard.jsx
+++ src/components/RevenueCard.jsx
@@ -4,24 +4,31 @@ export const RevenueCard = ({
     orderCount
 }) => {
     return <div className="bg-white-500 rounded shadow-md p-4">
-        <div className="text-gray-700">
-            {title}
-            ?
+        <div className="text-gray-700 flex">
+            <div>{title}</div>
+            <div className="ml-1 flex justify-center flex-col">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94"/>
+                </svg>
+            </div>
         </div>
-        <div className="flex justify-between">
-            <div>
+        <div className="flex justify-between pt-2">
+            <div className="font-bold text-2xl">
             ₹ {amount}
             </div>
             <div>
-                {orderCount ? <div className="flex">
-                    <div className="text-blue-500">
-                        {orderCount} Order(s)
+                {orderCount ? <div className="flex font-normal flex flex-row justify-center">
+                    <div className="flex">
+                        <div className="text-blue-500">
+                            {orderCount} Order(s)
+                        </div>
+                        <div className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                    <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
-                </svg>
-
-
                 </div> : null}
             </div>
         </div>
